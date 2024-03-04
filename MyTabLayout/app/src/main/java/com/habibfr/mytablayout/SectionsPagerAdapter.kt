967b80ca7e526d9e5a359bbb4c198728a19aaf31
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2024-03-04 12:49:35 +0700

    menambah tab layout dengan bundle

--- MyTabLayout/app/src/main/java/com/habibfr/mytablayout/SectionsPagerAdapter.kt
+++ MyTabLayout/app/src/main/java/com/habibfr/mytablayout/SectionsPagerAdapter.kt
@@ -1,5 +1,6 @@
 package com.habibfr.mytablayout
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentPagerAdapter
@@ -7,17 +8,32 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
 
-    override fun createFragment(position: Int): Fragment {
-        var fragment: Fragment? = null
-        when(position){
-            0 -> fragment = HomeFragment()
-            1 -> fragment = ProfileFragment()
-        }
+    //    override fun createFragment(position: Int): Fragment {
+//        var fragment: Fragment? = null
+//        when(position){
+//            0 -> fragment = HomeFragment()
+//            1 -> fragment = ProfileFragment()
+//        }
+//
+//        return fragment as Fragment
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return 2
+//    }
 
-        return fragment as Fragment
-    }
 
     override fun getItemCount(): Int {
-        return 2
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        val fragment:Fragment = HomeFragment()
+
+        fragment.arguments = Bundle().apply {
+            putInt(HomeFragment.ARG_SECTION_NUMBER, position + 1)
+        }
+
+        return fragment
     }
 }
