ec9560e6988be531a79ec288595ba3dec3fdf8c1
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2024-03-14 22:28:50 +0700

    add TaskStackBuilder

--- SimpleNotif/app/src/main/java/com/habibfr/simplenotif/MainActivity.kt
+++ SimpleNotif/app/src/main/java/com/habibfr/simplenotif/MainActivity.kt
@@ -12,6 +12,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.NotificationCompat
+import androidx.core.app.TaskStackBuilder
 import com.habibfr.simplenotif.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -42,32 +43,76 @@ class MainActivity : AppCompatActivity() {
         binding.btnSendNotification.setOnClickListener {
             sendNotification(title, message)
         }
+
+        binding.btnOpenDetail.setOnClickListener{
+            val detailIntent = Intent(this@MainActivity, DetailActivity::class.java)
+            detailIntent.putExtra(DetailActivity.EXTRA_TITLE, title)
+            detailIntent.putExtra(DetailActivity.EXTRA_MESSAGE, message)
+            startActivity(detailIntent)
+        }
     }
 
     private fun sendNotification(title: String, message: String) {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://hafarou.my.id"))
-        val pendingIntent = PendingIntent.getActivity(
-            this, 0, intent, PendingIntent.FLAG_IMMUTABLE
-        )
 
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID).setContentTitle(title)
-            .setSmallIcon(R.drawable.baseline_alarm_24).setContentText(message)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT).setContentIntent(pendingIntent)
+        val notifDetailIntent = Intent(this, DetailActivity::class.java)
+        notifDetailIntent.putExtra(DetailActivity.EXTRA_TITLE, title)
+        notifDetailIntent.putExtra(DetailActivity.EXTRA_MESSAGE, message)
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(notifDetailIntent)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                getPendingIntent(NOTIFICATION_ID, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+            } else {
+                getPendingIntent(NOTIFICATION_ID, PendingIntent.FLAG_UPDATE_CURRENT)
+            }
+        }
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(title)
+            .setSmallIcon(R.drawable.baseline_alarm_24)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setSubText(getString(R.string.notification_subtext))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
-                CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
             )
             builder.setChannelId(CHANNEL_ID)
             notificationManager.createNotificationChannel(channel)
         }
-
         val notification = builder.build()
         notificationManager.notify(NOTIFICATION_ID, notification)
     }
+
+//    private fun sendNotification(title: String, message: String) {
+//        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://hafarou.my.id"))
+//        val pendingIntent = PendingIntent.getActivity(
+//            this, 0, intent, PendingIntent.FLAG_IMMUTABLE
+//        )
+//
+//        val notificationManager =
+//            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//        val builder = NotificationCompat.Builder(this, CHANNEL_ID).setContentTitle(title)
+//            .setSmallIcon(R.drawable.baseline_alarm_24).setContentText(message)
+//            .setPriority(NotificationCompat.PRIORITY_DEFAULT).setContentIntent(pendingIntent)
+//            .setSubText(getString(R.string.notification_subtext))
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            val channel = NotificationChannel(
+//                CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT
+//            )
+//            builder.setChannelId(CHANNEL_ID)
+//            notificationManager.createNotificationChannel(channel)
+//        }
+//
+//        val notification = builder.build()
+//        notificationManager.notify(NOTIFICATION_ID, notification)
+//    }
 
     companion object {
         private const val NOTIFICATION_ID = 1
