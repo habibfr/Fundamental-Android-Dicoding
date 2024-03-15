e5616539bedb3bd2e97aca11668f72bc2137a565
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2024-03-15 15:51:14 +0700

    implemenet moshi

--- MyWorkManager/app/build.gradle.kts
+++ MyWorkManager/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -28,11 +29,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures{
         viewBinding = true
@@ -52,4 +53,7 @@ dependencies {
 
     implementation("androidx.work:work-runtime:2.8.1")
     implementation("com.loopj.android:android-async-http:1.4.10")
+
+    implementation("com.squareup.moshi:moshi-kotlin:1.11.0")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.8.0")
 }
